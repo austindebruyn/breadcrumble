d7cb057764aac49aaee5d45053c8bd301a2801c0
Austin deBruyn <austin.debruyn@gmail.com>
2016-01-02 15:44:39 -0800

    Partial hrefs working

--- test/acceptance.js
+++ test/acceptance.js
@@ -1,42 +1,71 @@
+'use strict';
 
-var expect = require('chai').expect;
+var _            = require('underscore');
 var breadcrumble = require('..');
+var config       = require('./config');
+var expect       = require('chai').expect;
 
-var config = {
-  name: 'Root',
-  routes: [
-    {
-      fragment: 'users',
-      name: 'Users',
-      routes: [
-        {
-          fragment: ':id',
-          name: 'ID (:id)',
-          routes: [
-            {
-              fragment: 'profile',
-              name: 'Profile'
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
-
-describe('parsing', function () {
+describe('parsing params and regexes', function () {
   var crumbs;
+
   beforeEach(function () {
-    breadcrumble.config = config;
-    crumbs = breadcrumble.match('/users/33/profile');
+    breadcrumble.config = config.paramAndRegex;
   });
 
-  it('should be an array of length 3', function () {
+  describe('with a param', function () {
+    beforeEach(function () {
+      crumbs = breadcrumble.match('/users/33/profile');
+    });
+
+    it('should be an array of length 4', function () {
+      expect(crumbs).to.be.an.instanceOf(Array);
+      expect(crumbs).to.have.length(4);
+    });
+
+    it('should contain the names of matched routes', function () {
+      expect(_.pluck(crumbs, 'name')).to.eql([ 'Root', 'Users', 'ID (33)', 'Profile' ]);
+    });
+
+    it('should be able to route to the param', function () {
+      expect(crumbs[2].href).to.eql('/users/33/');
+    });
+  });
+
+  describe('with a regex', function () {
+    beforeEach(function () {
+      crumbs = breadcrumble.match('/users/adebruyn/friends');
+    });
+
+    it('should be an array of length 4', function () {
+      expect(crumbs).to.be.an.instanceOf(Array);
+      expect(crumbs).to.have.length(4);
+    });
+
+    it('should contain the matched regex', function () {
+      expect(_.pluck(crumbs, 'name')).to.eql([ 'Root', 'Users', 'Username (adebruyn)', 'Friends List' ]);
+    })
+  });
+});
+
+describe('with other properties', function () {
+  var crumbs;
+
+  beforeEach(function () {
+    breadcrumble.config = config.withHrefs;
+    crumbs = breadcrumble.match('/app/pages/legal/');
+  });
+
+  it('should be an array of length 4', function () {
     expect(crumbs).to.be.an.instanceOf(Array);
-    expect(crumbs).to.have.length(3);
+    expect(crumbs).to.have.length(4);
   });
 
-  it('should contain the names of matched routes', function () {
-    expect(crumbs).to.eql([ 'Users', 'ID (33)', 'Profile' ]);
+  it('should contain the `other` property for each crumb', function () {
+    expect(_.pluck(crumbs, 'other')).to.eql([
+      undefined,
+      'This is a subtitle!',
+      3.14,
+      { nda: 'required' }
+    ]);
   });
 });
