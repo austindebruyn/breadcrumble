a52d06773939d8131a43011309815f64b2b0b4e5
Austin deBruyn <austin.debruyn@gmail.com>
2016-01-02 13:15:12 -0800

    MVP

--- test/acceptance.js
+++ test/acceptance.js
@@ -2,34 +2,41 @@
 var expect = require('chai').expect;
 var breadcrumble = require('..');
 
-var routes = [
-  {
-    fragment: 'users',
-    name: 'Users',
-    routes: [
-      {
-        fragment: ':id',
-        name: 'ID (:id)',
-        routes: [
-          {
-            fragment: 'profile',
-            name: 'Profile'
-          }
-        ]
-      }
-    ]
-  }
-];
+var config = {
+  name: 'Root',
+  routes: [
+    {
+      fragment: 'users',
+      name: 'Users',
+      routes: [
+        {
+          fragment: ':id',
+          name: 'ID (:id)',
+          routes: [
+            {
+              fragment: 'profile',
+              name: 'Profile'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
 
 describe('parsing', function () {
   var crumbs;
   beforeEach(function () {
-    breadcrumble.routes = routes;
+    breadcrumble.config = config;
     crumbs = breadcrumble.match('/users/33/profile');
   });
 
   it('should be an array of length 3', function () {
     expect(crumbs).to.be.an.instanceOf(Array);
     expect(crumbs).to.have.length(3);
+  });
+
+  it('should contain the names of matched routes', function () {
+    expect(crumbs).to.eql([ 'Users', 'ID (33)', 'Profile' ]);
   });
 });
