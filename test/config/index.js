b47beb6c0e245f9535ea70f55b03247637c88f03
Austin deBruyn <austin.debruyn@gmail.com>
2016-01-03 11:45:48 -0800

    Add missing file

---
+++ test/config/index.js
@@ -0,0 +1,60 @@
+
+var exports = module.exports;
+
+exports.paramAndRegex = {
+  name: 'Root',
+  routes: [
+    {
+      key: 'users',
+      name: 'Users',
+      routes: [
+        {
+          key: /[a-zA-Z]+/,
+          name: 'Username ($0)',
+          routes: [
+            {
+              key: 'friends',
+              name: 'Friends List'
+            }
+          ]
+        },
+        {
+          key: ':id',
+          name: 'ID (:id)',
+          routes: [
+            {
+              key: 'profile',
+              name: 'Profile'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+exports.withHrefs = {
+  name: 'Root',
+  routes: [
+    {
+      key: 'app',
+      name: 'App',
+      other: 'This is a subtitle!',
+      routes: [
+        {
+          key: 'pages',
+          name: 'Pages',
+          other: 3.14,
+          routes: [
+            {
+              key: 'legal',
+              name: 'Legal',
+              other: { nda: 'required' }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
